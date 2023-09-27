78d0686b4e4391d444e7fddde07d6f4c8e160766
ShivashisBhowmick23 <shiashis.bhowmick@outlook.com>
2023-09-28 01:20:26 +0530

    CarBoot

---
+++ src/main/java/com/boot/car/model/Car.java
@@ -0,0 +1,19 @@
+package com.boot.car.model;
+
+import lombok.Data;
+
+@Data
+public class Car {
+    private String carId;
+
+    private String carName;
+
+    private String carModel;
+
+    private String carType;
+
+    private double carPrice;
+
+    private int userRating;
+    private int totalCars;
+}
